b54fd796b725156629bccce55071c41eff1751fe
khshahed <khshahed@users.noreply.github.com>
2018-10-15 15:47:45 +0600

    Client server two way communication working.

--- src/networking/RunnableClient.java
+++ src/networking/RunnableClient.java
@@ -1,6 +1,9 @@
 package networking;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class RunnableClient implements Runnable {
@@ -9,8 +12,25 @@ public class RunnableClient implements Runnable {
         try {
             Socket socket = new Socket("localhost",6666);
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF("Hello from client");
-            dataOutputStream.flush();
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+//            dataOutputStream.writeUTF("Hello from client");
+//            dataOutputStream.flush();
+//            dataOutputStream.close();
+//            socket.close();
+
+            String messageFromServer, replay = "";
+            while (!replay.equals("stop")) {
+                System.out.println("Client: ");
+                replay = bufferedReader.readLine();
+                dataOutputStream.writeUTF(replay);
+                dataOutputStream.flush();
+
+                messageFromServer = dataInputStream.readUTF();
+                System.out.println("Client -- Message from Server -> " + messageFromServer);
+            }
+            System.out.println("### Closing Client ###");
             dataOutputStream.close();
             socket.close();
         } catch (Exception e) {
--- src/networking/RunnableServer.java
+++ src/networking/RunnableServer.java
@@ -14,9 +14,26 @@ public class RunnableServer implements Runnable {
             ServerSocket serverSocket = new ServerSocket(6666);
             Socket socket = serverSocket.accept();
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-            String message = (String) dataInputStream.readUTF();
-            System.out.println("Server -- Message -> " + message);
+//            String message = (String) dataInputStream.readUTF();
+//            System.out.println("Server -- Message -> " + message);
+//            serverSocket.close();
+
+            String messageFromClient = "", reply;
+            while (!messageFromClient.equals("stop")) {
+                messageFromClient = dataInputStream.readUTF();
+                System.out.println("Server -- Message from client -> " + messageFromClient);
+
+                System.out.println("Server: ");
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+            }
+            System.out.println("*** Closing Server ***");
+            dataInputStream.close();
+            socket.close();
             serverSocket.close();
         } catch (Exception e) {
             System.out.println("*************************************");
